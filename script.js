b52f705aa8a2ec727eae7bbcf2e32b2ad0a8a05e
Thecoder45674 <farah.liban.abdi@gmail.com>
2025-02-04 20:50:20 +1100

    feat: add library skeleton

---
+++ script.js
@@ -0,0 +1,9 @@
+const myLibrary = [];
+
+function Book(title, author, noOfPage, read) {
+    // the constructor
+}
+
+function addBookToLibrary (....) {
+    // take params, create a book then store it in the array
+}
