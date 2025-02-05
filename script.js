e9c5b7bcc8940a68bdd662bac0b9d255b72b2f07
Thecoder45674 <farah.liban.abdi@gmail.com>
2025-02-05 17:25:18 +1100

    feat: add book display logic and initial dummy book

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-const myLibrary = [];
+const bookContainer= document.querySelector('.book-list');
 
+// Function Constructor for Books
 function Book(title, author, noOfPage, read) {
-    // the constructor
+    this.title =  title;
+    this.author = author;
+    this.noOfPage = noOfPage;
+    this.read = read;
 }
 
-function addBookToLibrary (....) {
-    // take params, create a book then store it in the array
+const myLibrary = [
+    new Book("Harry Potter", "JK Rowling", 400, true),
+];
+
+// Function to Add Books to the book container
+function addBookToLibrary (books) {
+
+    books.forEach(book => {
+        let bookItem = document.createElement("div");
+        bookItem.classList.add("book-item");
+
+        bookItem.setAttribute("data-item", book.title);
+        bookItem.innerHTML = `<h3>${book.title}</h3><p>By ${book.author}</p><p>${book.noOfPage} pages</p>`;
+
+        bookContainer.appendChild(bookItem);
+    });
 }
+
+addBookToLibrary(myLibrary);
+
+
+
+
