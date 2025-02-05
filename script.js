87985dec30da6e56b5180090347249e97504f54f
Thecoder45674 <farah.liban.abdi@gmail.com>
2025-02-05 18:52:04 +1100

    feat: add close dialog btn functionality

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 const bookContainer= document.querySelector('.book-list');
-const showDialogBtn = document.getElementById("open-dialog-btn");
-const dialog = document.getElementById('dialog');
+const showDialogBtn = document.getElementById('open-dialog-btn');
+const dialog = document.getElementById('add-book-dialog');
+const addBtn = document.getElementById('add-book-btn');
+const closeBtn = document.getElementById('close-dialog-btn');
 
 const myLibrary = [
     new Book("Harry Potter", "JK Rowling", 400, true),
 ];
 
 // Function Constructor for Books
-function Book(title, author, noOfPage, read) {
+function Book(title, author, numberOfPage, isRead) {
     this.title =  title;
     this.author = author;
-    this.noOfPage = noOfPage;
-    this.read = read;
+    this.numberOfPage = numberOfPage;
+    this.isRead = isRead;
 }
 
-// Function to Add Books to the book container
-function addBookToLibrary (books) {
-
+// Function to display books to the book container
+function displayBooks(books) {
     books.forEach(book => {
         let bookItem = document.createElement("div");
         bookItem.classList.add("book-item");
 
         bookItem.setAttribute("data-item", book.title);
-        bookItem.innerHTML = `<h3>${book.title}</h3><p>By ${book.author}</p><p>${book.noOfPage} pages</p>`;
+        bookItem.innerHTML = `<h3>${book.title}</h3><p>By ${book.author}</p><p>${book.numberOfPage} pages</p>`; // Fix later 
 
         bookContainer.appendChild(bookItem);
     });
@@ -34,6 +35,16 @@ if (showDialogBtn) {
         dialog.showModal();
     });
 }
+
+// Close Dialog
+if (closeBtn) {
+    closeBtn.addEventListener("click", () => {
+        dialog.close();
+    });
+}
+
+// 
+
 
 // Add books to the library
 addBookToLibrary(myLibrary);
